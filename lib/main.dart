d5946dd363af0a51929e65ef1a98d0025902b2a3
fabiander <133480119+fabiander@users.noreply.github.com>
2025-05-10 23:09:49 -0500

    prueba 321

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter casa de pruebas',
+      title: 'Flutter casa de locos Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('haz  click infinidad de veces :'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
